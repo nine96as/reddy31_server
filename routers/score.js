1f27f5d531b6087e06debb4f7e00cfd5d262c3a5
nine96as <ashrafsaid79@gmail.com>
2023-12-18 13:32:22 +0000

    Format file

--- routers/score.js
+++ routers/score.js
@@ -4,12 +4,10 @@ const scoreController = require('../controllers/score.js');
 
 const scoreRouter = Router();
 
-scoreRouter.get("/", scoreController.index);
+scoreRouter.get('/', scoreController.index);
 scoreRouter.get('/token/:token', scoreController.show);
 scoreRouter.get('/subject/:subject', scoreController.showBySubject);
-scoreRouter.post("/", scoreController.create);
-scoreRouter.patch("/:token", scoreController.update);
-
-
+scoreRouter.post('/', scoreController.create);
+scoreRouter.patch('/:token', scoreController.update);
 
 module.exports = scoreRouter;
