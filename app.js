3d1a55b34e9f76e9fa3850739266d1d4b079fa3b
Esham <Esham156@gmail.com>
2023-12-18 15:24:25 +0000

    add answer routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const userRouter = require('./routers/user');
 const scoreRouter = require('./routers/score');
 const subjectRouter = require('./routers/subject');
 const noteRouter = require('./routers/note');
+const answerRouter = require('./routers/answer');
+
 
 const app = express();
 
@@ -23,5 +25,7 @@ app.use('/users', userRouter);
 app.use('/scores', scoreRouter);
 app.use('/subjects', subjectRouter);
 app.use('/notes', noteRouter);
+app.use('/answers', answerRouter);
+
 
 module.exports = app;
---
+++ controllers/answer.js
@@ -0,0 +1,40 @@
+const Answer = require('../models/answer')
+
+//get by question
+
+async function showByQuestion(req, res) {
+    try {
+      const questionId = req.params.questionId; 
+  
+      const answers = await Answer.find({ questionId });
+  
+      res.json(answers);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+//create
+
+async function create(req, res) {
+    try {
+      const { text, isCorrect, questionId } = req.body;
+  
+      const newAnswer = new Answer({
+        text,
+        isCorrect: isCorrect || false, 
+        questionId
+      });
+  
+      const savedAnswer = await newAnswer.save();
+  
+      res.status(201).json(savedAnswer);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+}
+  
+module.exports = {
+    create,
+    showByQuestion
+};
