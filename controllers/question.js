df255a09c5193b7be81c9c151503a8be00518713
nine96as <ashrafsaid79@gmail.com>
2023-12-18 20:45:30 +0000

    Complete generateQuestions() logic, add refs in create(), complete destroy() logic

--- controllers/question.js
+++ controllers/question.js
@@ -1,4 +1,63 @@
+require('dotenv').config();
+const { OpenAI } = require('openai');
 const Question = require('../models/question');
+const Note = require('../models/note');
+const Subject = require('../models/subject');
+const Answer = require('../models/answer');
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+const generateQuestions = async (subject, notes) => {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'assistant',
+        content: `You are an assistive revision tool for school students, where your task is to generate revision questions based on the provided topic and the revision notes of the student. 
+        
+        You should output the questions in a JSON format, with the below template to be strictly followed:
+
+        questions: [
+            {
+                name: 'Fill in question name',
+                answers: [
+                    {
+                    text: 'Fill in answer value',
+                    isCorrect: false (true if it is the correct answer)
+                    },
+                    {
+                    text: 'Fill in answer value',
+                    isCorrect: false (true if it is the correct answer)
+                    },
+                    {
+                    text: 'Fill in answer value',
+                    isCorrect: false (true if it is the correct answer)
+                    },
+                    {
+                    text: 'Fill in answer value',
+                    isCorrect: false (true if it is the correct answer)
+                    }
+                ]
+            }
+        ];
+
+        Do not add any backticks to the outputted JSON array. Do not wrap the 'questions' JSON array in curly brackets.
+
+        There should only be four possible answers within a given question, with only one answer having the 'isCorrect' key set to 'true'. Feel free to generate an appropriate amount of questions depending on the length of the revision notes.
+
+        The topic is: ${subject}
+    
+        The revision notes are as follows:
+    
+        ${notes.map((n) => `${n.content}\n`)}`
+      }
+    ],
+    model: 'gpt-3.5-turbo-1106'
+  });
+
+  return completion.choices[0];
+};
 
 const index = async (req, res) => {
   try {
@@ -12,6 +71,68 @@ const index = async (req, res) => {
   }
 };
 
+const create = async (req, res) => {
+  let success = false;
+  while (!success) {
+    try {
+      const { subjectId } = req.body;
+
+      const subject = await Subject.findById(subjectId);
+      const notes = await Note.find({ subjectId: subjectId });
+
+      if (!notes) {
+        return res.status(400).json({
+          error: 'You have no notes with the inputted subject'
+        });
+      }
+
+      const { message } = await generateQuestions(subject.name, notes);
+      const { questions } = JSON.parse(message.content);
+      if (questions) success = true;
+
+      const filteredQuestions = questions.map((q) => {
+        return {
+          name: q.name,
+          subjectId: subjectId
+        };
+      });
+
+      const createdQuestions = await Question.create(filteredQuestions);
+
+      const filteredAnswers = [];
+
+      for (let i = 0; i < questions.length; i++) {
+        for (let j = 0; j < questions[i].answers.length; j++) {
+          filteredAnswers.push({
+            text: questions[i].answers[j].text,
+            isCorrect: questions[i].answers[j].isCorrect,
+            questionId: createdQuestions[i]._id
+          });
+        }
+      }
+
+      const createdAnswers = await Answer.create(filteredAnswers);
+
+      res
+        .status(201)
+        .json({ questions: createdQuestions, answers: createdAnswers });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+};
+
+const destroy = async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(200).json(await Question.findByIdAndDelete(id));
+  } catch (e) {
+    res.status(404).json({ error: e.message });
+  }
+};
+
 module.exports = {
-  index
+  index,
+  create,
+  destroy
 };
