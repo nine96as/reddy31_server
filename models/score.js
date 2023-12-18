e3af7e5909b757fa635b22e50c7523c0c5ba273d
nine96as <ashrafsaid79@gmail.com>
2023-12-18 10:23:29 +0000

    Create Score model

---
+++ models/score.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const scoreSchema = mongoose.Schema(
+  {
+    value: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    subjectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Subject',
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const Score = mongoose.model('Score', scoreSchema);
+
+module.exports = Score;
