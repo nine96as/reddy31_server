66a800eda685f8b3162ce7fb4dc60788e0eef0d2
nine96as <ashrafsaid79@gmail.com>
2023-12-18 13:50:06 +0000

    Format file

--- routers/subject.js
+++ routers/subject.js
@@ -4,10 +4,8 @@ const subjectController = require('../controllers/subject');
 
 const subjectRouter = Router();
 
-subjectRouter.get("/", subjectController.index);
-subjectRouter.post("/", subjectController.create);
-subjectRouter.patch("/:id", subjectController.destroy);
-
-
+subjectRouter.get('/', subjectController.index);
+subjectRouter.post('/', subjectController.create);
+subjectRouter.patch('/:id', subjectController.destroy);
 
 module.exports = subjectRouter;
