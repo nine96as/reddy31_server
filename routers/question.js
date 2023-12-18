31dfeaf2627a282e6e65a91f0d0c60a8f9b94802
nine96as <ashrafsaid79@gmail.com>
2023-12-18 20:46:08 +0000

    Add create(), destroy() refs in Question router

--- routers/question.js
+++ routers/question.js
@@ -5,5 +5,7 @@ const questionController = require('../controllers/question');
 const questionRouter = Router();
 
 questionRouter.get('/', questionController.index);
+questionRouter.post('/', questionController.create);
+questionRouter.delete('/:id', questionController.destroy);
 
 module.exports = questionRouter;
