d3d0d7e8fc3a6facf809da64d0771ccaba732f92
nine96as <ashrafsaid79@gmail.com>
2023-12-19 20:39:38 +0000

    Ensure 100% cov in answer unit tests

--- __tests__/unit/controllers/answer.spec.js
+++ __tests__/unit/controllers/answer.spec.js
@@ -34,52 +34,57 @@ describe('answer controller', () => {
     it('should handle errors', async () => {
       const mockReq = { params: { questionId: '123' } };
 
-      jest.spyOn(Answer, 'find').mockRejectedValue(new Error('Error fetching answers'));
+      jest
+        .spyOn(Answer, 'find')
+        .mockRejectedValue(new Error('Error fetching answers'));
 
       await answerController.showByQuestion(mockReq, mockRes);
 
       expect(Answer.find).toHaveBeenCalledWith({ questionId: '123' });
       expect(mockStatus).toHaveBeenCalledWith(500);
-      expect(mockJson).toHaveBeenCalledWith({ error: 'Error fetching answers' });
+      expect(mockJson).toHaveBeenCalledWith({
+        error: 'Error fetching answers'
+      });
     });
   });
 
   describe('create', () => {
     it('should create a new answer successfully', async () => {
-        const newAnswer = {
-          text: 'New Answer',
-          isCorrect: true,
-          questionId: '456'
-        };
-      
-        const savedAnswer = { _id: '3', ...newAnswer };
-      
-        const mockReq = { body: newAnswer };
-      
-        jest.spyOn(Answer, 'create').mockResolvedValue(savedAnswer);
-      
-        await answerController.create(mockReq, mockRes);
-      
-        expect(Answer.create).toHaveBeenCalledWith(newAnswer);
-        expect(mockStatus).toHaveBeenCalledWith(201);
-        expect(mockJson).toHaveBeenCalledWith(savedAnswer);
-    });
-      
-
-    it('should handle errors during answer creation', async () => {
       const newAnswer = {
         text: 'New Answer',
         isCorrect: true,
         questionId: '456'
       };
 
+      const savedAnswer = { _id: '3', ...newAnswer };
+
       const mockReq = { body: newAnswer };
 
-      jest.spyOn(Answer.prototype, 'save').mockRejectedValue(new Error('Error creating answer'));
+      jest.spyOn(Answer, 'create').mockResolvedValue(savedAnswer);
 
       await answerController.create(mockReq, mockRes);
 
-      expect(Answer).toHaveBeenCalledWith(newAnswer);
+      expect(Answer.create).toHaveBeenCalledWith(newAnswer);
+      expect(mockStatus).toHaveBeenCalledWith(201);
+      expect(mockJson).toHaveBeenCalledWith(savedAnswer);
+    });
+
+    it('should handle errors during answer creation', async () => {
+      const newAnswer = {
+        text: 'New Answer',
+        isCorrect: false,
+        questionId: '456'
+      };
+
+      const mockReq = { body: newAnswer };
+
+      jest
+        .spyOn(Answer, 'create')
+        .mockRejectedValue(new Error('Error creating answer'));
+
+      await answerController.create(mockReq, mockRes);
+
+      expect(Answer.create).toHaveBeenCalledWith(newAnswer);
       expect(mockStatus).toHaveBeenCalledWith(500);
       expect(mockJson).toHaveBeenCalledWith({ error: 'Error creating answer' });
     });
