4c6f766d0329d1b16ac801bca65678dd017cfa7b
nine96as <ashrafsaid79@gmail.com>
2023-12-19 22:04:23 +0000

    Format file, add status code 200 to successful resps

--- controllers/subject.js
+++ controllers/subject.js
@@ -5,7 +5,7 @@ const Subject = require('../models/subject');
 async function index(req, res) {
   try {
     const subjects = await Subject.find({});
-    res.json({
+    res.status(200).json({
       count: subjects.length,
       data: subjects
     });
@@ -20,13 +20,11 @@ async function create(req, res) {
   try {
     const { name } = req.body;
 
-    const newSubject = new Subject({
-      name
+    const subject = await Subject.create({
+      name: name
     });
 
-    const savedSubject = await newSubject.save();
-
-    res.status(201).json(savedSubject);
+    res.status(201).json(subject);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -38,13 +36,13 @@ async function destroy(req, res) {
   try {
     const subjectId = req.params.id;
 
-    const deletedSubject = await Subject.findByIdAndRemove(subjectId);
+    const deletedSubject = await Subject.findByIdAndDelete(subjectId);
 
     if (!deletedSubject) {
       return res.status(404).json({ error: 'Subject not found' });
     }
 
-    res.json(deletedSubject);
+    res.status(200).json(deletedSubject);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
